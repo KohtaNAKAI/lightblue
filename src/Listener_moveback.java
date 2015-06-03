b9696e4f79d5dd8e641e83954f93af66e05c7201
KohtaNAKAI <kokko1219@gmail.com>
2015-06-03 19:42:28 +0800

    Operation Error... trial.

--- src/Listener_moveback.java
+++
@@ -1,7 +0,0 @@
-import java.awt.event.*;
-
-public class Listener_moveback implements ActionListener{
-	public void actionPerformed(ActionEvent e){	
-		Lightblue2.history.move_back();
-	}
-}
--- src/Listener_restart_from_latest.java
+++
@@ -1,8 +0,0 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class Listener_restart_from_latest implements ActionListener{
-	public void actionPerformed(ActionEvent e){	
-		Lightblue2.history.restart_from_latest();
-	}
-}
