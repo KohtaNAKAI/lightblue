0019f499ecad17abb50e12e1f24b3fe95d6f0ae1
Kouta Nakai <kokko1219@gmail.com>
2015-06-06 19:49:27 +0800

    re-arranged console

--- src/Console.java
+++ src/Console.java
@@ -17,21 +17,25 @@ public class Console{
 		Container cont = Console.getContentPane();
 		cont.setLayout(null);
 		
+		//1step
 		JButton JB_1step = new JButton("Move Selection : 1 step");
 		cont.add(JB_1step);
 		JB_1step.setBounds(10,10,200,50);
 		JB_1step.addActionListener(new Listener_available_moves());
 		
+		//2steps
 		JButton JB_2step = new JButton("Move Selection : 2 steps");
 		cont.add(JB_2step);
 		JB_2step.setBounds(10,70,200,50);
 		JB_2step.addActionListener(new Listener_available_moves2());
 		
+		//4steps
 		JButton JB_4step = new JButton("Move Selection : 4 steps");
 		cont.add(JB_4step);
 		JB_4step.setBounds(10,130,200,50);
 		JB_4step.addActionListener(new Listener_available_moves4());
 		
+		//Move Piece
 		JLabel JL_player = new JLabel("player");
 		cont.add(JL_player);
 		JL_player.setBounds(250,10,50,30);
@@ -66,20 +70,21 @@ public class Console{
 		JB_move.setBounds(300,130,100,50);
 		JB_move.addActionListener(new Listener_move_from_GUI());
 		
+		//N turn search
 		JButton JB_Nturn = new JButton("N turns");
 		cont.add(JB_Nturn);
-		JB_Nturn.setBounds(10,200,100,50);
+		JB_Nturn.setBounds(10,190,100,50);
 		JB_Nturn.addActionListener(new Listener_search_moves());
 		cont.add(JT_num);
-		JT_num.setBounds(110,200,100,50);
+		JT_num.setBounds(110,190,100,50);
 		
 		//History
-		Scroll.setBounds(10, 300, 300, 240);
+		Scroll.setBounds(10, 250, 300, 210);
 		cont.add(Scroll);
 	    
 		JButton JB_move_back = new JButton("back");
 		cont.add(JB_move_back);
-		JB_move_back.setBounds(320,300,100,50);
+		JB_move_back.setBounds(320,250,100,50);
 		JB_move_back.addActionListener(new Listener_moveback());
 		
 		Console.setBounds(500, 0 , 450, 500);
